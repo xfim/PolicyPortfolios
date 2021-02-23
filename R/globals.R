9bf0322982353e5958104cd721cdb16d1039be7b
Xavier Fernández i Marín <xavier.fim@gmail.com>
2021-02-23 20:08:25 +0100

    Ensure that globals.R contains all used functions.

--- R/globals.R
+++ R/globals.R
@@ -7,6 +7,11 @@ if (base::getRversion() >= "2.15.1") {
     "comparative", "dev_type_html", # from pp_report()
     "pdf", "dev.off", # from grDevices, for pp_plot()
     "na.omit", # from stats() in pp_clean()
+    "N", # from pp_similarity()
+    "Country.destination", "Year.destination", # from pp_similarity()
+    "Method", "Method.label", # from pp_similarity()
+    "value", # from pp_similarity()
+    "error", # from pp_similarity()
     "browseURL" # from utils, for pp_plot()
   ))
 }
