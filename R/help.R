afe1fbbb865f05afd69c0f9a733e19a9026387d0
Xavier Fernández i Marín <xavier.fim@gmail.com>
2021-02-23 20:10:19 +0100

    Add functions used.

--- R/help.R
+++ R/help.R
@@ -1,13 +1,13 @@
 #' Tools for managing, measuring and visualizing policy portfolios
 #'
-#' PolicyPortfolios simplifies the creation of data structures suitable
+#' Tools for simplifying the creation and management of data structures suitable
 #' for dealing with policy portfolios, that is, two-dimensional spaces of
-#' of policy instruments and policy targets. It allows to generates measures of
-#' their characteristics and facilitates its visualization.
+#' of policy instruments and policy targets. It allows to generate measures of
+#' portfolio characteristics and facilitates their visualization.
 #'
-#' @references \url{http://xavier-fim.net/packages/PolicyPortfolios}.
+#' @references \url{http://xavier-fim.net/packages/PolicyPortfolios/}.
 #' @import ggplot2
-#' @importFrom dplyr tibble as_tibble filter mutate bind_rows case_when arrange %>%
+#' @importFrom dplyr tibble as_tibble filter mutate bind_rows case_when arrange n summarize group_by ungroup %>%
 #' @importFrom vegan diversity
 #' @importFrom ineq Gini
 #' @importFrom mgcv uniquecombs
