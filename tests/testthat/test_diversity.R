73debfc28f59ef6315812efd2b10c5411fa310f2
Xavier Fernández i Marín <xavier.fim@gmail.com>
2021-02-23 20:09:11 +0100

    Create a new test for calculating diversity.

---
+++ tests/testthat/test_diversity.R
@@ -0,0 +1,18 @@
+require(testthat)
+data(consensus)
+context("Check that diversity (AID) calculation matches APSR")
+
+calc.aid <- consensus %>%
+  filter(Sector == "Environmental") %>%
+  filter(Country %in% c("France", "United States")) %>%
+  filter(Year %in% c(1976, 2005)) %>%
+  pp_array() %>%
+  apply(., c(1, 2, 3), diversity) %>%
+  as.vector() %>%
+  round(digits = 3)
+
+test_that("diversity matches reported in APSR", {
+            apsr.aid <- c(0.464, 0.839, 0.855, 0.850) # 1976 (FR, US), 2005 (FR, US)
+            expect_identical(calc.aid, apsr.aid)
+})
+
