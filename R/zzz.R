472b1974417f8b1603726f83793d20af891773b8
Xavier Fernández i Marín <xavier.fim@gmail.com>
2021-02-24 11:06:52 +0100

    Include a message when loading the package, to facilitate finding out its proper citation.

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+# .onLoad <- function(libname, pkgname) 
+# {
+#   library.dynam("PolicyPortfolios", pkgname, libname)
+# }
+
+PolicyPortfoliosStartupMessage <- function()
+{
+  msg <- c(paste0("PolicyPortfolios version ", 
+           packageVersion("PolicyPortfolios")),
+           "\nType 'citation(\"PolicyPortfolios\")' for citing this R package or the datasets in publications.")
+  return(msg)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  # startup message
+  msg <- PolicyPortfoliosStartupMessage()
+  if(!interactive())
+    msg[1] <- paste("Package 'PolicyPortfolios' version", packageVersion("PolicyPortfolios"))
+  packageStartupMessage(msg)      
+  invisible()
+}
