cb1eda4fac282b586aa8c35836ec963db37f247d
Xavier Fernández i Marín <xavier.fim@gmail.com>
2021-02-26 20:14:15 +0100

    Check references.

--- R/help.R
+++ R/help.R
@@ -6,6 +6,8 @@
 #' portfolio characteristics and facilitates their visualization.
 #'
 #' @references \url{http://xavier-fim.net/packages/PolicyPortfolios/}.
+#' @references Fernández-i-Marín, X., Knill, C. & Steinebach, Y. (2021). Studying Policy Design Quality in Comparative Perspective. _American Political Science Review_, online first.
+#' @references Knill, C., Schulze, K. & Tosun, J. (2012). Regulatory Policy Outputs and Impacts. Exploring a Complex Relationship. _Regulation & Governance_, 5(4), 427-444. doi:10.1111/j.1748-5991.2012.01150.x.
 #' @import ggplot2
 #' @importFrom dplyr tibble as_tibble filter mutate bind_rows case_when arrange n summarize group_by ungroup %>%
 #' @importFrom vegan diversity
